f9363a1964a0f298c18914f9b2e8bd4125c58dab
paragkoche <paragkoche0@gmail.com>
2024-04-25 08:18:45 +0530

    bug fix

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,7 +1,9 @@
 import { DataSource } from "typeorm";
+import { Users } from "./users";
 
 export default new DataSource({
   type: "sqlite",
   database: "new.db",
   synchronize: true,
+  entities: [Users],
 });
