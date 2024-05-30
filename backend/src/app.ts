eadcff41917fa308bacd6525e6d10cdf0bbb3229
Yash Gajbhiye <77782972+ARYANEON@users.noreply.github.com>
2024-05-30 13:24:16 +0530

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,13 @@ import express from "express";
 import UsersPath from "@/router/users";
 import FilesPath from "@/router/file";
 import cookieParser from "cookie-parser";
+import cros from 'cors';
+
 const app = express();
+app.use(cros({
+    origin: process.env.NEXT_PUBLIC_API_BASE_URL,
+    credentials: true
+}))
 app.use(express.json());
 app.use(cookieParser());
 app.use("/users", UsersPath);
