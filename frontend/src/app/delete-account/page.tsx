701706f05709c3b97f7daf16192a55026ae1ded4
Yash Gajbhiye <77782972+ARYANEON@users.noreply.github.com>
2024-06-06 16:25:22 +0530

    completed
    delete account update ui

--- frontend/src/app/delete-account/page.tsx
+++ frontend/src/app/delete-account/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import { redirect } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 
@@ -40,22 +34,25 @@ const DeleteAccount = () => {
   };
 
   return (
-    <main className="text-center h-screen flex justify-center items-center">
-      <Box sx={{ minWidth: 275 }}>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography variant="h5" component="div">
-              Your ID: {data}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button onClick={deleteAccount} size="large" variant="contained">
-              Delete My Account
-            </Button>
-          </CardActions>
-        </Card>
-      </Box>
-    </main>
+    <div className="h-screen flex justify-center items-center ">
+      <div className="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-indigo-100 ">
+        <div className="">
+          <div className="text-center p-5 flex-auto justify-center">
+            {" "}
+            <h2 className="text-xl font-bold py-4 ">Are you sure?</h2>
+            <p className="text-sm text-gray-500 px-8">
+              Do you really want to delete your account? This process cannot be
+              undone
+            </p>
+          </div>
+          <div className="p-3  mt-2 text-center space-x-4 md:block">
+            <button onClick={deleteAccount} className="mb-2 md:mb-0 bg-indigo-500 border border-indigo-500 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg hover:bg-indigo-600">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
